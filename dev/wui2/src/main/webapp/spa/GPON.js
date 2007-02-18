c67b1656bf7a945c3f247ced8038bae6689f8eb4
d-schulz <d-schulz>
2007-02-18 02:50:07 +0000

    *** empty log message ***

--- dev/wui2/src/main/webapp/spa/GPON.js
+++ dev/wui2/src/main/webapp/spa/GPON.js
@@ -8,6 +8,50 @@ var GPON = {
 		
 		this._lastid_++;
 		return this._lastid_;
+	},
+    encode_utf8: function (rohtext)
+	{
+	// dient der Normalisierung des Zeilenumbruchs
+	rohtext = rohtext.replace(/\r\n/g,"\n");
+	var utftext = "";
+	for(var n=0; n<rohtext.length; n++)
+		{
+		// ermitteln des Unicodes des  aktuellen Zeichens
+		var c=rohtext.charCodeAt(n);
+		// alle Zeichen von 0-127 => 1byte
+		if (c<128)
+			utftext += String.fromCharCode(c);
+		// alle Zeichen von 127 bis 2047 => 2byte
+		else if((c>127) && (c<2048)) {
+			utftext += String.fromCharCode((c>>6)|192);
+			utftext += String.fromCharCode((c&63)|128);}
+		// alle Zeichen von 2048 bis 66536 => 3byte
+		else {
+			utftext += String.fromCharCode((c>>12)|224);
+			utftext += String.fromCharCode(((c>>6)&63)|128);
+			utftext += String.fromCharCode((c&63)|128);}
+		}
+	return utftext;
+	},
+    decode_utf8: function (utftext)
+	{
+	var plaintext = ""; var i=0; var c=c1=c2=0;
+	// while-Schleife, weil einige Zeichen uebersprungen werden
+	while(i<utftext.length)
+		{
+		c = utftext.charCodeAt(i);
+		if (c<128) {
+			plaintext += String.fromCharCode(c); i++;}
+		else if((c>191) && (c<224)) {
+			c2 = utftext.charCodeAt(i+1);
+			plaintext += String.fromCharCode(((c&31)<<6) | (c2&63));
+			i+=2;}
+		else {
+			c2 = utftext.charCodeAt(i+1); c3 = utftext.charCodeAt(i+2);
+			plaintext += String.fromCharCode(((c&15)<<12) | ((c2&63)<<6) | (c3&63));
+			i+=3;}
+		}
+	return plaintext;
 	}
 };
 
@@ -103,7 +147,7 @@ GPON.PropHandle = new Class(
 	  var containerNode = new Element('div');
 	  var node = null;
 	  
-	  if (this.pType == 'longtext') 
+	  if (this.pType == 'longtext' || this.pType == 'htmltext') 
 	  {
 	   node = new Element('textarea');
 	   node.rows=10;
@@ -137,6 +181,12 @@ GPON.PropHandle = new Class(
 	  {
 	    var node = new Element('pre');
 	    node.appendText(this.pValue);
+	    node.injectInside(containerNode);
+	  }
+	  else if(this.pType=='htmltext') 
+	  {
+	    var node = new Element('div');
+	    node.setHTML(this.pValue);
 	    node.injectInside(containerNode);
 	  }
 	  else {
@@ -181,10 +231,13 @@ GPON.Form = new Class({
 	  save.injectInside(formControlBar);
 	  save.setHTML('save');
 	  save.onclick = this.toOutput.bindWithEvent(this);
+	  var formMainPanel = new Element('div');
+	  formMainPanel.addClass('formMainPanelInput');
+	  formMainPanel.injectInside(formNode);
 	  // table
 	  // form fields
 	  var tableNode = new Element('table');
-	  tableNode.injectInside(formNode);
+	  tableNode.injectInside(formMainPanel);
 	  var tbodyNode = new Element('tbody');
 	  tbodyNode.injectInside(tableNode);
 	  for (i=0; i < this.propHandles.length; i++) 
@@ -204,6 +257,8 @@ GPON.Form = new Class({
 	    inputNode = this.propHandles[i].createInputNode(this);
 	    inputNode.injectInside(cellNode);
 	  }
+	  
+	  formNode.dragHandle=formControlBar;
 	  return formNode;
 	},
 	asOutputDisplayElement: function() 
@@ -222,9 +277,13 @@ GPON.Form = new Class({
 	  edit.injectInside(formControlBar);
 	  edit.setHTML('edit');
 	  edit.onclick = this.toInput.bindWithEvent(this);
+	  // main
+	  var formMainPanel = new Element('div');
+	  formMainPanel.addClass('formMainPanelOutput');
+	  formMainPanel.injectInside(formNode);
 	  // table
 	  var tableNode = new Element('table');
-	  tableNode.injectInside(formNode);
+	  tableNode.injectInside(formMainPanel);
 	  var tbodyNode = new Element('tbody');
 	  tbodyNode.injectInside(tableNode);
 	  for (i=0; i < this.propHandles.length; i++) 
@@ -244,6 +303,7 @@ GPON.Form = new Class({
 	    inputNode = this.propHandles[i].createOutputNode(this);
 	    inputNode.injectInside(cellNode);
 	  }
+	  formNode.dragHandle=formControlBar;	  
 	  return formNode;
 	},
 	putData: function(propHandle, newValue) 
@@ -252,14 +312,30 @@ GPON.Form = new Class({
 	},
 	toOutput: function(event)
 	{
+	  // send data to server
+	  var str = Json.toString(this.propHandles);
+	  
+	  var ajaxOptions = 
+	  {
+	    method: 'post',
+	    postBody: str,
+	    onComplete: function() { alert('oncomplete');}
+	  }
+	  
+	  var myAjax = new Ajax('http://localhost:8080/wui2/ajax/spa.do', 
+	                        ajaxOptions
+	                        ).request();
+	
 	  var myOutputDOM = this.asOutputDisplayElement();
 	  $(this.containerId+'_input').replaceWith(myOutputDOM);
+      myOutputDOM.getParent().effect('margin',{duration: 10}).start(0,10);
 	  return false;
 	},
 	toInput: function(event)
 	{
 	  var myInputDOM = this.asInputFormElement();
 	  $(this.containerId+'_output').replaceWith(myInputDOM);
+	  myInputDOM.getParent().effect('margin').start(10,0);
 	  return false;
 	}
 });
